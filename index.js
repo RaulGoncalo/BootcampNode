64dae74a61e23e1ebafb70e932c67ac4d7211885
RaulGoncalo <raulgoncalo.98@gmail.com>
2022-06-25 08:10:10 -0300

    Modelo de exportação e importação - ESModules - add op3

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import operacoes from './operacoes.js';
 import multiplicacao from './operacoes2.js';
+import {divisao, resto} from './operacoes3.js';
 
 console.log(operacoes.teste)
 console.log(operacoes.soma(4, 5));
 console.log(operacoes.subtracao(6, 3));
-console.log(multiplicacao(3, 9))
+console.log(multiplicacao(3, 9));
+console.log(divisao(25,5));
+console.log(resto(100, 3));
