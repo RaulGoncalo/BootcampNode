1d97564c7f6b3016485fb8d0e0972ef59c8da9ca
RaulGoncalo <raulgoncalo.98@gmail.com>
2022-07-17 10:49:28 -0300

    Desafio modulo 2 - implementar o oauth com o facebook

--- app.js
+++ app.js
@@ -4,9 +4,22 @@ const express         =     require('express')
   , session           =     require('express-session')
   , bodyParser        =     require('body-parser')
   , config            =     require('./configuration/config')
+  ,facebookConfig     =     require('./configuration/facebookConfig')
   , app               =     express();
 
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 passport.use(new GoogleStrategy({
     clientID: config.api_key,
@@ -18,14 +31,17 @@ passport.use(new GoogleStrategy({
   }
 ));
 
-// Passport session setup.
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+passport.use(new FacebookStrategy({
+    clientID: facebookConfig.api_key,
+    clientSecret: facebookConfig.api_secret,
+    callbackURL: facebookConfig.callback_url
+  },
+  function(accessToken, refreshToken, profile, done){
+    return done(null, profile);
+  }
+));
 
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
+
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -40,10 +56,23 @@ app.get('/', function(req, res){
   res.render('index', { user: req.user });
 });
 
+
+//google
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
 app.get('/auth/google/callback', 
   passport.authenticate('google',  { successRedirect : '/', failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+
+//facebook
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook',  { successRedirect : '/', failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   }
---
+++ configuration/facebookConfig.js
@@ -0,0 +1,6 @@
+module.exports={
+    "api_key": "ID do Aplicativo",
+    "api_secret": "Chave Secreta do Aplicativo",
+    "callback_url": "http://localhost:3000/auth/facebook/callback"
+}
+            
